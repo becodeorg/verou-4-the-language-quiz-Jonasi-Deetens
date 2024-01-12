d474b73c88d66a54609f848dd69e10e50609e1dd
Jonasi-Deetens <jonasideetens@hotmail.com>
2024-01-12 14:10:51 +0100

    Add letter cleaner

--- rain.js
+++ rain.js
@@ -1,5 +1,6 @@
 const main = document.getElementById("main");
 let rainDrops = [];
+let explodedLetters = [];
 const words = [
     'avontuur',
     'vieren',
@@ -110,7 +111,59 @@ const randomColor = () => {
     const color = `rgb(${red}, ${green}, ${blue})`;
   
     return color;
-  };
+};
+
+const fly = (originalXPos, originalYPos) => {
+    document.querySelectorAll('.letter').forEach(letterElement => {
+        let xPos = parseInt(originalXPos) + getRandomInt(-50, 50);
+        let yPos = parseInt(originalYPos) + getRandomInt(-50, 50);
+        letterElement.style.transition = 'left 2s, top 2s';
+        letterElement.style.left = xPos + "px";
+        while (yPos >= window.innerHeight) yPos = parseInt(originalYPos) + getRandomInt(-50, 50);
+        letterElement.style.top = yPos + "px";
+    });
+}
+
+const explodeText = (element) => {
+    let xPos = parseInt(element.style.left.split("px")[0]) + 8;
+    const rect = element.getBoundingClientRect();
+    let yPos = rect.top - 18;
+    
+    let letters = element.textContent.split("");
+
+    main.removeChild(element);
+    letters.forEach(letter => {
+        let letterElement = document.createElement("p");
+        letterElement.textContent = letter;
+        letterElement.style.left = xPos + "px";
+        letterElement.style.top = yPos + "px";
+        letterElement.style.color = randomColor();
+        letterElement.classList.add("letter");
+
+        yPos += 17;
+        explodedLetters.push(letterElement);
+        main.appendChild(letterElement);
+    });
+    fly(xPos, yPos);
+};
+
+const checkBottom = () => {
+    rainDrops.forEach(rain => {
+        const rect = rain.getBoundingClientRect();
+        if ((rect.bottom - rect.height / 20000)>= window.innerHeight) {
+            //rain.style.top = window.innerHeight - rect.width + "px";
+            //rain.classList.add("horizontal");
+            explodeText(rain);
+        }
+    });
+
+    if (explodedLetters.length >= 500) {
+        let lettersToDelete = explodedLetters.splice(0, explodedLetters.length/2);
+        lettersToDelete.forEach(letter => {
+            main.removeChild(letter);
+        });
+    }
+};
 
 const letItRain = () => {
     let rain = document.createElement("p");
@@ -122,24 +175,11 @@ const letItRain = () => {
 
     rainDrops.push(rain);
     main.appendChild(rain);
-}
-
-const checkBottom = () => {
-    rainDrops.forEach(rain => {
-        const rect = rain.getBoundingClientRect();
-        if ((rect.bottom - rect.height / 2)>= window.innerHeight) {
-            rain.style.top = window.innerHeight - rect.width + "px";
-            rain.classList.add("horizontal");
-        }
-    });
-
-    if (rainDrops.length >= 500) {
-        let rainDropsToDelete = rainDrops.splice(0, rainDrops.length/2);
-        rainDropsToDelete.forEach(drop => {
-            main.removeChild(drop);
-        });
-    }
 };
 
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+setInterval(checkBottom, 1);
 setInterval(letItRain, 500);
-setInterval(checkBottom, 100);
